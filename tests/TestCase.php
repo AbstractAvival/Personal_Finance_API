d846b05149a9c059341faec634abcb8848f312d1
Avival <chrisFJ.avival@gmail.com>
2025-06-04 22:41:44 -0600

    Configured base TestCase class setUp and tearDown methods, added baseUri attribute to base TestCase class.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,22 @@
 
 namespace Tests;
 
+use DB;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected $baseUri = '/api';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        DB::beginTransaction();
+    }
+
+    protected function tearDown(): void
+    {
+        DB::rollBack();
+        parent::tearDown();
+    }
 }
