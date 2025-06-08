99b1d8e3121f3cf8ef541ad160022dc53e287bd2
Avival <chrisFJ.avival@gmail.com>
2025-06-07 19:43:45 -0600

    Added /user route to api.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\UserController;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,5 +8,9 @@ use Illuminate\Support\Facades\Route;
 Route::controller( UserController::class )->prefix( "users" )->group(
     function () {
         Route::get( "", "index" )->name( "users.index" );
+        Route::get( "/{userId}", "show" )->name( "users.show" );
+        Route::delete( "/{userId}", "delete" )->name( "users.delete" );
+        Route::post( "", "store" )->name( "users.store" );
+        Route::patch( "/{userId}", "update" )->name( "users.update" );
     }
 );
