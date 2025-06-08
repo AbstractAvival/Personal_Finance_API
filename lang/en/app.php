393cdea353a201a890b4bccc293b467e9b1a097c
Avival <chrisFJ.avival@gmail.com>
2025-06-08 13:32:24 -0600

    Added laravel localization. Created app.php in lang folder to handle string localizations.

---
+++ lang/en/app.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    "conflict" => "The provided data or solicited operation conflicts with current status.",
+    "no_record_found" => "The requested record could not be found.",
+    "no_records_found" => "The requested records could not be found.",
+    "record_deleted" => "Record deleted successfully.",
+    "record_not_deleted" => "Error deleting record.",
+    "record_created" => "Record created successfully.",
+    "record_updated" => "Record updated successfully.",
+    "record_not_updated" => "Error updating record.",
+    "duplicate_record" => "A matching record already exists in the database."
+];
